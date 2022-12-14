d4c96189fcde4032b404d048b8a825edbdeb380d
Xmo <18851989097@163.com>
2022-12-14 17:53:50 +0800

    build: release v0.5.0-beta.0

--- test/config.ts
+++ test/config.ts
@@ -3,10 +3,10 @@ export const choicesA = [
   ['A', 'B', 'C', 'D'],
   ['A', 'B', 'C', 'D'],
   ['A', 'B', 'C', 'D']
-];
+] as const;
 
 export const choicesB = [
   ['option1A', 'option1B', 'option1C'],
   ['option2A', 'option2B', 'option2C', 'option2D'],
   ['option3A', 'option3B']
-];
+] as const;
